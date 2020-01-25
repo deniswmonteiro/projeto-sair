160342dafe9c7494f2932fe21e7850e1e670cd64
Denis Monteiro <deniswmonteiro@gmail.com>
2020-01-25 14:59:08 -0300

    Projeto Automaçao, Heroku

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             // 'database' => 'heroku_06wk9pfg',
 
             'driver' => 'mongodb',
-            'dsn'=>'mongodb+srv://denismonteiro:ep10806702@clusterprojetoautomacao-sn33g.mongodb.net/test?retryWrites=true&w=majority',
+            'dsn'=>'mongodb+srv://denismonteiro:ep10806702@clusterprojetoautomacao-sn33g.mongodb.net/projetoautomacao?retryWrites=true&w=majority',
             'database' => 'projetoautomacao',
         ],
 
