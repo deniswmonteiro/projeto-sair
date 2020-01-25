07ed571b5e79d978b6c141b912814d99b6f549a4
Denis Monteiro <deniswmonteiro@gmail.com>
2020-01-25 11:47:28 -0300

    Projeto Automaçao, Heroku

--- config/database.php
+++ config/database.php
@@ -35,13 +35,17 @@ return [
 
     'connections' => [
         'mongodb' => [
-            'driver'   => 'mongodb',
-            'host'     => env('MONGO_DB_HOST', 'localhost'),
-            'port'     => env('MONGO_DB_PORT', 27017),
-            'database' => env('MONGO_DB_DATABASE', 'projetoautomacao'),
-            'username' => env('MONGO_DB_USERNAME', ''),
-            'password' => env('MONGO_DB_PASSWORD', ''),
-            'options'  => []
+            // 'driver'   => 'mongodb',
+            // 'host'     => env('MONGO_DB_HOST', 'localhost'),
+            // 'port'     => env('MONGO_DB_PORT', 27017),
+            // 'database' => env('MONGO_DB_DATABASE', 'projetoautomacao'),
+            // 'username' => env('MONGO_DB_USERNAME', ''),
+            // 'password' => env('MONGO_DB_PASSWORD', ''),
+            // 'options'  => []
+
+            'driver' => 'mongodb',
+            'dsn'=>'mongodb+srv://denismonteiro:ep10806702@clusterprojetoautomacao-sn33g.mongodb.net/projetoautomacao?retryWrites=true&w=majority',
+            'database' => 'projetoautomacao',
         ],
 
         'sqlite' => [
