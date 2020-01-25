22d241367a7a8a31c5838ba3c498a24ebd0161b3
Denis Monteiro <deniswmonteiro@gmail.com>
2020-01-25 13:57:25 -0300

    Projeto Automaçao, Heroku

--- config/database.php
+++ config/database.php
@@ -38,7 +38,7 @@ return [
     'connections' => [
         'mongodb' => [
             'driver'   => 'mongodb',
-            'host'     => env('MONGO_DB_HOST'),
+            'host'     => env('MONGO_DB_HOST', $DATABASE_URL),
             'port'     => env('MONGO_DB_PORT', 27017),
             'database' => env('MONGO_DB_DATABASE', 'projetoautomacao'),
             'username' => env('MONGO_DB_USERNAME', 'denismonteiro'),
