7bb0f67f0cea398bccc993e6d58a9d8e5ed27acc
Denis Monteiro <deniswmonteiro@gmail.com>
2020-01-25 12:48:39 -0300

    Projeto Automaçao, Heroku

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL=parse_url('mongodb+srv://denismonteiro:ep10806702@clusterprojetoautomacao-sn33g.mongodb.net/projetoautomacao?retryWrites=true&w=majority');
+// $DATABASE_URL=parse_url('mongodb+srv://denismonteiro:ep10806702@clusterprojetoautomacao-sn33g.mongodb.net/projetoautomacao?retryWrites=true&w=majority');
 
 return [
 
@@ -37,17 +37,17 @@ return [
 
     'connections' => [
         'mongodb' => [
-            'driver'   => 'mongodb',
-            'host'     => env('MONGO_DB_HOST', $DATABASE_URL),
-            'port'     => env('MONGO_DB_PORT', 27017),
-            'database' => env('MONGO_DB_DATABASE', 'projetoautomacao'),
-            'username' => env('MONGO_DB_USERNAME', 'denismonteiro'),
-            'password' => env('MONGO_DB_PASSWORD', 'ep10806702'),
-            'options'  => []
+            // 'driver'   => 'mongodb',
+            // 'host'     => env('MONGO_DB_HOST', 'localhost'),
+            // 'port'     => env('MONGO_DB_PORT', 27017),
+            // 'database' => env('MONGO_DB_DATABASE', 'projetoautomacao'),
+            // 'username' => env('MONGO_DB_USERNAME', 'denismonteiro'),
+            // 'password' => env('MONGO_DB_PASSWORD', 'ep10806702'),
+            // 'options'  => []
 
-            // 'driver' => 'mongodb',
-            // 'dsn'=>'mongodb+srv://denismonteiro:ep10806702@clusterprojetoautomacao-sn33g.mongodb.net/projetoautomacao?retryWrites=true&w=majority',
-            // 'database' => 'projetoautomacao',
+            'driver' => 'mongodb',
+            'dsn'=>'mongodb+srv://denismonteiro:ep10806702@clusterprojetoautomacao-sn33g.mongodb.net/projetoautomacao?retryWrites=true&w=majority',
+            'database' => 'projetoautomacao',
         ],
 
         'sqlite' => [
