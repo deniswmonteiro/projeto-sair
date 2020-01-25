73b8baf1913f530928c64cb1c1f2009036733e6e
Denis Monteiro <deniswmonteiro@gmail.com>
2020-01-25 20:52:00 -0300

    Projeto Automação, MongoDB Atlas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Projeto Automação &bull; {{$paginaAtual}}</title>
     <link rel="stylesheet" href="{{asset('/css/uikit.css')}}">
