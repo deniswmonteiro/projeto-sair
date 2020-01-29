8821cd98729a65536e5bb538d37d1b152cebed5a
Denis Monteiro <deniswmonteiro@gmail.com>
2020-01-29 19:56:46 -0300

    Projeto Automaçao

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,13 +1,13 @@
 <!-- modal logout -->
 <div id="{{$id}}" uk-modal>
-  <div class="uk-modal-dialog uk-modal-body modal">
+  <div class="uk-modal-dialog uk-modal-body uk-border-rounded modal">
     <h3 class="uk-modal-title">{{$texto}}</h3>
-    <div class="uk-text-right uk-margin-remove-bottom btn">
+    <div class="uk-flex uk-flex-right uk-margin-remove-bottom btn">
       <a class="uk-button uk-flex-last@s btn-modal-confirmar" onclick="event.preventDefault(); document.querySelector('form.logout').submit()">{{__('Confirmar')}}</a>
       <form action="{{route('logout')}}" class="logout" method="post" style="display:none;">
           @csrf
       </form>
-      <a class="uk-button uk-modal-close btn-cancelar" type="button">
+      <a class="uk-button uk-modal-close uk-margin-right btn-cancelar" type="button">
         Cancelar
       </a>
     </div>
