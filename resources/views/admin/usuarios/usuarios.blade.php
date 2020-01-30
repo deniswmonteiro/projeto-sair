e988e2989911e47308021257fcc9c3adcdd692fe
Denis Monteiro <deniswmonteiro@gmail.com>
2020-01-30 16:09:04 -0300

    Projeto Automaçao

--- resources/views/admin/usuarios/usuarios.blade.php
+++ resources/views/admin/usuarios/usuarios.blade.php
@@ -37,12 +37,16 @@
               <td>
                 <div class="uk-flex-button btn">
                   <a href="{{route('usuarios.editar', ['usuario' => $usuario->id])}}" class="uk-button uk-margin-right btn-acao">
-                    {{__('Atualizar')}}
+                    {{-- {{__('Atualizar')}} --}}
+                    <i class="material-icons-round">update</i>
                   </a>
                   <form action="{{route('usuarios.destroy', ['usuario' => $usuario->id])}}" method="post" class="uk-display-inline-block form-btn-submit">
                     @csrf
                     @method("delete")
-                    <button type="submit" class="uk-button uk-width-1-1 btn-cancelar">{{__('Remover')}}</button>
+                    <button type="submit" class="uk-button uk-width-1-1 btn-cancelar">
+                      <i class="material-icons-round">delete</i>
+                      {{-- {{__('Remover')}} --}}
+                    </button>
                   </form>
                 </div>
               </td>
