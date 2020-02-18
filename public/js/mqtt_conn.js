3a1979fea5892d2e0f6cd64d248b264d6b84c19e
Denis Monteiro <deniswmonteiro@gmail.com>
2020-02-18 15:09:54 -0300

    Projeto Automaçao

--- public/js/mqtt_conn.js
+++ public/js/mqtt_conn.js
@@ -1,37 +1,33 @@
-let mqtt =
-    "mosquitto_sub -h tailor.cloudmqtt.com -p 12564 -v -t '#' -u ldwjwmao -P 'SZmxi8fOtKC8'";
+import mqtt from "mqtt";
 console.log(mqtt);
 
-// var mqtt = require("mqtt");
-// console.log(mqtt);
+var options = {
+    port: 12564,
+    clientId:
+        "mqttjs_" +
+        Math.random()
+            .toString(16)
+            .substr(2, 8),
+    username: "ldwjwmao",
+    password: "SZmxi8fOtKC8"
+};
 
-// var options = {
-//     port: 12564,
-//     clientId:
-//         "mqttjs_" +
-//         Math.random()
-//             .toString(16)
-//             .substr(2, 8),
-//     username: "ldwjwmao",
-//     password: "SZmxi8fOtKC8"
-// };
+var client = connect("mqtt://tailor.cloudmqtt.com", options);
 
-// var client = connect("mqtt://tailor.cloudmqtt.com", options);
+client.on("connect", function() {
+    // When connected
 
-// client.on("connect", function() {
-//     // When connected
+    // subscribe to a topic
+    client.subscribe("topic1/test", function() {
+        // when a message arrives, do something with it
+        client.on("message", function(topic, message, packet) {
+            console.log("Received '" + message + "' on '" + topic + "'");
+        });
+    });
 
-//     // subscribe to a topic
-//     client.subscribe("topic1/test", function() {
-//         // when a message arrives, do something with it
-//         client.on("message", function(topic, message, packet) {
-//             console.log("Received '" + message + "' on '" + topic + "'");
-//         });
-//     });
-
-//     // publish a message to a topic
-//     client.publish("topic1/test", "IoT test message", function() {
-//         console.log("Message is published");
-//         //client.end(); // Close the connection when published
-//     });
-// });
+    // publish a message to a topic
+    client.publish("topic1/test", "IoT test message", function() {
+        console.log("Message is published");
+        //client.end(); // Close the connection when published
+    });
+});
