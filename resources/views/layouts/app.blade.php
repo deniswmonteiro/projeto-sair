ca85952979d16936d2659df7f42a346de4b26716
Denis Monteiro <deniswmonteiro@gmail.com>
2020-03-27 16:41:33 -0300

    Projeto Automação

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Projeto Automação &bull; {{$paginaAtual}}</title>
     <link rel="stylesheet" href="{{secure_asset('css/uikit.css')}}" type="text/css">
@@ -10,6 +10,7 @@
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
     <link rel="stylesheet" href="{{secure_asset('css/app.css')}}" type="text/css">
     <link rel="shortcut icon" href="{{secure_asset('logo.ico')}}" type="image/x-icon">
+    <link rel="manifest" href="{{secure_asset('manifest.json')">
     <script src="{{secure_asset('js/jquery-3.4.1.min.js')}}" type="text/javascript"></script>
     <script src="{{secure_asset('js/jquery.mask.js')}}" type="text/javascript"></script>
     <script src="{{secure_asset('js/mqttws31.min.js')}}" type="text/javascript"></script>
