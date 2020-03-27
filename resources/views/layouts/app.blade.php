f719f65c869f7c0d112e10f3246cc7d966327214
Denis Monteiro <deniswmonteiro@gmail.com>
2020-03-27 11:40:09 -0300

    Projeto Automação

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <link href="https://fonts.googleapis.com/css?family=Quicksand:500,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
     <link rel="stylesheet" href="{{secure_asset('css/app.css')}}">
+    <link rel="shortcut icon" href="{{secure_asset('logo.ico')}}" type="image/x-icon">
     <script src="{{secure_asset('js/jquery-3.4.1.min.js')}}" type="text/javascript"></script>
     <script src="{{secure_asset('js/jquery.mask.js')}}" type="text/javascript"></script>
     <script src="{{secure_asset('js/mqttws31.min.js')}}" type="text/javascript"></script>
