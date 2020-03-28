7c50ad059b1d4f9c0205ab2e2bb282fca896ecb1
Denis Monteiro <deniswmonteiro@gmail.com>
2020-03-27 22:52:40 -0300

    Projeto Automação

--- config/app.php
+++ config/app.php
@@ -52,10 +52,9 @@ return [
     |
     */
 
-	//'url' => env('APP_URL', 'http://localhost'),
+	'url' => env('APP_URL', 'http://localhost'),
 
-    'url' => env('APP_URL', 'https://projetoceamazon.herokuapp.com/'),
-	'asset_url' => env('ASSET_URL', null),
+    'asset_url' => env('ASSET_URL', null),
 
 	/*
     |--------------------------------------------------------------------------
