3631c14c21217927a2ba47d2fd71c4ac8195a00a
Denis Monteiro <deniswmonteiro@gmail.com>
2020-03-27 23:08:17 -0300

    Projeto Automação

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -53,7 +53,7 @@ self.addEventListener("fetch", event => {
 
 let deferredPrompt;
 
-window.addEventListener('beforeinstallprompt', (e) => {
+self.addEventListener('beforeinstallprompt', (e) => {
   // Prevent the mini-infobar from appearing on mobile
   e.preventDefault();
   // Stash the event so it can be triggered later.
