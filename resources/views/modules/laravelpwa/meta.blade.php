7d1f8d7f35aa756ea929340156afaecd9bc69dbc
Denis Monteiro <deniswmonteiro@gmail.com>
2020-03-27 21:54:05 -0300

    Projeto Automação

--- resources/views/modules/laravelpwa/meta.blade.php
+++ resources/views/modules/laravelpwa/meta.blade.php
@@ -32,19 +32,15 @@
 
 <script type="text/javascript">
     // Initialize the service worker
-    // if ('serviceWorker' in navigator) {
-    //     navigator.serviceWorker.register('/serviceworker.js', {
-    //         scope: '.'
-    //     }).then(function (registration) {
-    //         // Registration was successful
-    //         console.log('Laravel PWA: ServiceWorker registration successful with scope: ', registration.scope);
-    //     }, function (err) {
-    //         // registration failed :(
-    //         console.log('Laravel PWA: ServiceWorker registration failed: ', err);
-    //     });
-    // }
-
-    navigator.serviceWorker && navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-  console.log('SW funcionando, registrado com o escopo: ', registration.scope);
-});
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('/serviceworker.js', {
+            scope: '.'
+        }).then(function (registration) {
+            // Registration was successful
+            console.log('Laravel PWA: ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            // registration failed :(
+            console.log('Laravel PWA: ServiceWorker registration failed: ', err);
+        });
+    }
 </script>
