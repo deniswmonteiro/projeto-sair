77827f21b1b49b7c6ce25e8716b54b9d0e5ca9bf
deniswmonteiro <deniswmonteiro@gmail.com>
2020-07-08 15:16:12 -0300

    Projeto SAIR

--- resources/views/layouts/salas.blade.php
+++ resources/views/layouts/salas.blade.php
@@ -50,6 +50,6 @@
     </div>
   </section>
 
-  <script src="{{asset('js/plugins.min.js')}}" type="text/javascript"></script>
-  <script src="{{asset('js/gerenciamento-nuvem.js')}}"></script>
+  <script src="{{secure_asset('js/plugins.min.js')}}" type="text/javascript"></script>
+  <script src="{{secure_asset('js/gerenciamento-nuvem.js')}}"></script>
 @endsection
