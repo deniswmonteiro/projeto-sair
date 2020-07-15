147cd0ea62589891b5689a52312dad34f56a376e
deniswmonteiro <deniswmonteiro@gmail.com>
2020-07-15 11:43:14 -0300

    Projeto SAIR

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,22 +13,6 @@
   <link rel="shortcut icon" href="{{secure_asset('logo.ico')}}" type="image/x-icon">
   <link rel="manifest" href={{secure_asset('/manifest.json')}}>
   <script src="{{secure_asset('js/paho-mqtt.min.js')}}" type="text/javascript"></script>
-  <script>
-    // This is the "Offline page" service worker
-      /*
-        Add this below content to your HTML page inside a <script type="module"> <script> tag, or add the js file to your page at the very top to register service worker--}}
-        If you get an error about not being able to import, double check that you have type="module" on your <script /> tag
-      */
-
-      /*
-        This code uses the pwa-update web component https://github.com/pwa-builder/pwa-update to register your service worker, tell the user when there is an update available and let the user know when your PWA is ready to use offline.
-      */
-      import "https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate";
-
-      const el = document.createElement("pwa-update");
-      document.body.appendChild(el);
-      import '@pwabuilder/pwainstall'
-  </script>
 </head>
 
 <body>
@@ -87,6 +71,12 @@
 
   <script src="{{secure_asset('js/plugins.min.js')}}" type="text/javascript"></script>
   <script src="{{secure_asset('js/app.js')}}" type="text/javascript"></script>
+  <script type="module">
+    import "https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate";
+
+    const el = document.createElement("pwa-update");
+    document.body.appendChild(el);
+  </script>
 </body>
 
 </html>
