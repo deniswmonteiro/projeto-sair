f060b8bdd54def52a4a4f574a2a4f096153ce99d
deniswmonteiro <deniswmonteiro@gmail.com>
2020-07-15 11:19:11 -0300

    Projeto SAIR

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SAIR &bull; {{$paginaAtual}}</title>
-    <link href="https://fonts.googleapis.com/css?family=Quicksand:500,700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{secure_asset('css/plugins.min.css')}}">
-    <link rel="stylesheet" href="{{secure_asset('css/app.css')}}" type="text/css">
-    <link rel="shortcut icon" href="{{secure_asset('logo.ico')}}" type="image/x-icon">
-    <link rel="manifest" href={{secure_asset('/manifest.json')}}>
-    <script src="{{secure_asset('js/paho-mqtt.min.js')}}" type="text/javascript"></script>
-    <script>
-      // This is the "Offline page" service worker
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>SAIR &bull; {{$paginaAtual}}</title>
+  <link href="https://fonts.googleapis.com/css?family=Quicksand:500,700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="{{secure_asset('css/plugins.min.css')}}">
+  <link rel="stylesheet" href="{{secure_asset('css/app.css')}}" type="text/css">
+  <link rel="shortcut icon" href="{{secure_asset('logo.ico')}}" type="image/x-icon">
+  <link rel="manifest" href={{secure_asset('/manifest.json')}}>
+  <script src="{{secure_asset('js/paho-mqtt.min.js')}}" type="text/javascript"></script>
+  <script>
+    // This is the "Offline page" service worker
       /*
         Add this below content to your HTML page inside a <script type="module"> <script> tag, or add the js file to your page at the very top to register service worker--}}
         If you get an error about not being able to import, double check that you have type="module" on your <script /> tag
@@ -27,64 +28,65 @@
       const el = document.createElement("pwa-update");
       document.body.appendChild(el);
       import '@pwabuilder/pwainstall'
-    </script>
-  </head>
-  <body>
-    <pwa-install>
-    <div class="uk-text-center notificacao-alerta">
-      @include('flash::message')
+  </script>
+</head>
+
+<body>
+  <div class="uk-text-center notificacao-alerta">
+    @include('flash::message')
+  </div>
+  @yield("conteudo")
+
+  <footer class="rodape">
+    <div class="dados">
+      <div class="uk-container">
+        <div class="uk-child-width-1-2@s uk-grid-large" uk-grid>
+          <div class="contatos">
+            <div>
+              <h3>Contatos</h3>
+              <ul class="uk-list">
+                <li>
+                  <span>Endereço:</span> Parque de Ciência e Tecnologia do Guamá - Av. Perimetral, nº 2651, Prédio 01
+                  - CEP: 66077-830 - Guamá - Belém/PA
+                </li>
+                <li>
+                  <span>Email:</span> ceamazonufpa@gmail.com
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="logo-rodape">
+            <div class="uk-child-width-1-2@m uk-grid-column-small uk-flex-middle" uk-grid>
+              <div class="uk-text-right@m uk-text-center@s">
+                <a href="http://ceamazon.com.br">
+                  <img src="{{secure_asset('img/logo-ceamazon.svg')}}" alt="Logo do CEAMAZON">
+                </a>
+              </div>
+              <div class="uk-text-center@s">
+                <a href="https://portal.ufpa.br/">
+                  <img src="{{secure_asset('img/logo-ufpa.svg')}}" alt="Logo da UFPA">
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    @yield("conteudo")
-
-    <footer class="rodape">
-      <div class="dados">
-        <div class="uk-container">
-          <div class="uk-child-width-1-2@s uk-grid-large" uk-grid>
-            <div class="contatos">
-              <div>
-                <h3>Contatos</h3>
-                <ul class="uk-list">
-                  <li>
-                    <span>Endereço:</span> Parque de Ciência e Tecnologia do Guamá - Av. Perimetral, nº 2651, Prédio 01 - CEP: 66077-830 - Guamá - Belém/PA
-                  </li>
-                  <li>
-                    <span>Email:</span> ceamazonufpa@gmail.com
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="logo-rodape">
-              <div class="uk-child-width-1-2@m uk-grid-column-small uk-flex-middle" uk-grid>
-                <div class="uk-text-right@m uk-text-center@s">
-                  <a href="http://ceamazon.com.br">
-                    <img src="{{secure_asset('img/logo-ceamazon.svg')}}" alt="Logo do CEAMAZON">
-                  </a>
-                </div>
-                <div class="uk-text-center@s">
-                  <a href="https://portal.ufpa.br/">
-                    <img src="{{secure_asset('img/logo-ufpa.svg')}}" alt="Logo da UFPA">
-                  </a>
-                </div>
-              </div>
-            </div>
+    <div class="copyright uk-text-center">
+      <div class="uk-container">
+        <div uk-grid>
+          <div class="uk-margin-auto">
+            <p>
+              &copy; SAIR {{date('Y')}} &ndash; Todos os direitos reservados.
+            </p>
           </div>
         </div>
       </div>
-      <div class="copyright uk-text-center">
-        <div class="uk-container">
-          <div uk-grid>
-            <div class="uk-margin-auto">
-              <p>
-                &copy; SAIR {{date('Y')}} &ndash; Todos os direitos reservados.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
+    </div>
+  </footer>
 
-    <script src="{{secure_asset('js/plugins.min.js')}}" type="text/javascript"></script>
-    <script src="{{secure_asset('js/app.js')}}" type="text/javascript"></script>
-  </pwa-install> 
-  </body>
+  <script src="{{secure_asset('js/plugins.min.js')}}" type="text/javascript"></script>
+  <script src="{{secure_asset('js/app.js')}}" type="text/javascript"></script>
+</body>
+
 </html>
