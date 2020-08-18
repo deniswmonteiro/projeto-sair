7c9e718667e72a465bb76a32db78fd146ac0c78e
deniswmonteiro <deniswmonteiro@gmail.com>
2020-08-18 17:17:25 -0300

    Projeto SAIR

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
   <link rel="stylesheet" href="{{secure_asset('css/app.css')}}" type="text/css">
   <link rel="shortcut icon" href="{{secure_asset('logo.ico')}}" type="image/x-icon">
   <link rel="manifest" href={{secure_asset('/manifest.json')}}>
-  <script src="{{secure_asset('js/paho-mqtt.min.js')}}" type="text/javascript"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.js" type="text/javascript"></script>
 </head>
 
 <body>
